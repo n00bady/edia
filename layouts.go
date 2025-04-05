9b88a3d0d61cb18fc519b0191d61d109475e4654
n00bady <kaz00@vivaldi.net>
2025-04-05 03:03:41 +0300

    Added the layout for mobile devices, fixed the buttons a bit.

--- layouts.go
+++ layouts.go
@@ -9,14 +9,163 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func mobileForm(appState *AppState) (fyne.CanvasObject, error) {
-	soon := widget.NewLabel("Soon(tm)")
+	// soon := widget.NewLabel("Soon(tm)")
+	landlord_name := widget.NewEntry()
+	landlord_name.SetPlaceHolder("Εκμισθωτής")
 
-	body := container.NewVBox(soon)
+	renter_name := widget.NewEntry()
+	renter_name.SetPlaceHolder("Μισθωτής")
+
+	// I hate this
+	lats := make([]*widget.Entry, 4)
+	longs := make([]*widget.Entry, 4)
+	for i := range 4 {
+		lats[i] = widget.NewEntry()
+		lats[i].SetPlaceHolder(fmt.Sprintf("Πλάτος %d", i+1))
+		longs[i] = widget.NewEntry()
+		longs[i].SetPlaceHolder(fmt.Sprintf("Μήκος %d", i+1))
+	}
+
+	acres := widget.NewEntry()
+	acres.SetPlaceHolder("Στρέμματα")
+
+	t := widget.NewEntry()
+	t.SetPlaceHolder("Ειδος Καλ/γειας")
+
+	// Starting date input and it's button that opens a calendar for easier date choosing
+	start_input := widget.NewEntry()
+	start_input.SetPlaceHolder("ΑΠΟ")
+	startDateButton := widget.NewButton("Pick a date", func() {
+		showCalendar(start_input, appState.window)
+	})
+
+	// Same as starting date but for the ending date
+	end_input := widget.NewEntry()
+	end_input.SetPlaceHolder("ΕΩΣ")
+	endDateButton := widget.NewButton("Pick a date", func() {
+		showCalendar(end_input, appState.window)
+	})
+
+	r := widget.NewEntry()
+	r.SetPlaceHolder("Μίσθωμα")
+
+	// Save button
+	saveBtn := widget.NewButton("Αποθήκευση", func() {
+		// Convert to float64 and gather the coordinates
+		coords := make([]Coordinate, 0, 4)
+		for i := range 4 {
+			latValue, errLat := strconv.ParseFloat(lats[i].Text, 64)
+			longValue, errLon := strconv.ParseFloat(longs[i].Text, 64)
+
+			if errLat != nil || errLon != nil {
+				log.Printf("Error parsing coordinates")
+				dialog.ShowError(errLat, appState.window)
+			}
+
+			coords = append(coords, Coordinate{Latitude: latValue, Longitude: longValue})
+		}
+		// and the size
+		size, err := strconv.ParseFloat(acres.Text, 64)
+		if err != nil {
+			log.Printf("Error parsing the size of the land")
+			dialog.ShowError(err, appState.window)
+		}
+
+		money, err := strconv.ParseFloat(r.Text, 32)
+		if err != nil {
+			log.Printf("Error parsing the rent price")
+			dialog.ShowError(err, appState.window)
+		}
+
+		// We build the new entry here
+		newEntry := Entry{
+			LandlordName: landlord_name.Text,
+			RenterName:   renter_name.Text,
+			Coords:       coords,
+			Timestamp:    time.Now(),
+			Size:         size,
+			Type:         t.Text,
+			Start:        start_input.Text,
+			End:          start_input.Text,
+			Rent:         money,
+		}
+
+		err = saveEntry(appState.db, newEntry)
+		if err != nil {
+			log.Printf("Error saving entry: %v", err)
+			dialog.ShowError(err, appState.window)
+			return
+		}
+
+		log.Printf("Saved entry: %s\n%s\n%f\netc...\n", newEntry.LandlordName, newEntry.RenterName, newEntry.Rent)
+		dialog.ShowInformation("Database: ", fmt.Sprintf("Saved entry: %s\n%s\n%f\netc...\n", newEntry.LandlordName, newEntry.RenterName, newEntry.Rent), appState.window)
+	})
+
+	title := widget.NewLabel("EDIA")
+	title.Alignment = fyne.TextAlignCenter
+	coords_l := widget.NewLabel("Γεωγραφικές Συντεταγμένες")
+	separator := widget.NewSeparator()
+	duration := widget.NewLabel("Διαρκεια")
+
+	// Layout for the left container
+	left_container := container.NewVBox(
+		landlord_name,
+		renter_name,
+		coords_l,
+		lats[0],
+		longs[0],
+		separator,
+		lats[1],
+		longs[1],
+		separator,
+		lats[2],
+		longs[2],
+		separator,
+		lats[3],
+		longs[3],
+	)
+
+	// Layout for the right container
+	right_container := container.NewVBox(
+		acres,
+		t,
+		r,
+		duration,
+		start_input,
+		startDateButton,
+		end_input,
+		endDateButton,
+		separator,
+	)
+
+	backButton := widget.NewButton("Go back", func() {
+		tmp, err := mainView(appState)
+		if err != nil {
+			log.Printf("error constructing list layout: %v", err)
+		}
+		body := container.NewBorder(nil, nil, nil, nil, tmp)
+		appState.window.SetContent(body)
+	})
+
+	// body := container.NewVBox(soon, backButton)
+	body := container.NewVScroll(
+		container.NewVBox(
+			left_container,
+			right_container,
+			container.New(
+				layout.NewHBoxLayout(),
+				layout.NewSpacer(),
+				backButton,
+				saveBtn,
+			),
+		),
+	)
 
 	return body, nil
 }
@@ -201,7 +350,7 @@ func mainView(appState *AppState) (fyne.CanvasObject, error) {
 			appState.window.SetContent(tmp)
 		})
 	} else {
-		addButton = widget.NewButton("+", func() {
+		addButton = widget.NewButtonWithIcon("Add New Entry", theme.ContentAddIcon(), func() {
 			tmp, err := desktopForm(appState)
 			if err != nil {
 				log.Printf("error constructing desktop layout: %v", err)
@@ -210,8 +359,21 @@ func mainView(appState *AppState) (fyne.CanvasObject, error) {
 			appState.window.SetContent(body)
 		})
 	}
+	addButton.Resize(fyne.NewSize(200, 200))
 
-	body := container.NewBorder(nil, addButton, nil, nil, container.NewVScroll(list))
+	body := container.New(
+		layout.NewBorderLayout(nil, nil, nil, nil),
+		container.NewVScroll(list),
+		container.New(
+			layout.NewVBoxLayout(),
+			layout.NewSpacer(),
+			container.New(
+				layout.NewHBoxLayout(),
+				layout.NewSpacer(),
+				container.NewPadded(addButton),
+			),
+		),
+	)
 
 	return body, nil
 }
